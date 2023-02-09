383732c7a9a0683781e7afb57fcf93efb902c735
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-09 19:22:20 +0100

    get date now and time now

--- client/components/DateTime.jsx
+++ client/components/DateTime.jsx
@@ -1,16 +1,38 @@
 import { View, Text,StyleSheet } from 'react-native'
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import WeatherItem from './WeatherItem'
 
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
 const DateTime = () => {
+    const [date, setDate] = useState('')
+    const [time, setTime] = useState('')
+    useEffect (() => {
+        setInterval(() => {
+            const time = new Date();
+            const month = time.getMonth();
+            const date = time.getDate();
+            const day = time.getDay();
+            const hour = time.getHours();
+            const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+            const minutes = time.getMinutes();
+            const ampm = hour >=12 ? 'pm' : 'am'
+        
+            setTime((hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes) +ampm) 
+        
+            setDate(days[day] + ', ' + date+ ' ' + months[month]) 
+        
+        }, 1000);
+    }, [])
   return (
     <View style={styles.container}>
         <View>
             <View>
-                <Text style={styles.heading}>12:30pm</Text>
+                <Text style={styles.heading}>{time}</Text>
             </View>
             <View>
-                <Text style={styles.subHeading}>Monday, June 7</Text>
+                <Text style={styles.subHeading}>{date}</Text>
             </View>
             <View style={styles.weatherItemContainer}>
                 <WeatherItem/>
