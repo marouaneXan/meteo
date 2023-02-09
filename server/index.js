4dbe5b42e16db54aa28d7893213756a16be8ce5d
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-09 22:10:25 +0100

    connect with database successfully

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+
+//mongodb connection
+const connectDB = require("./Config/db");
+connectDB();
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
