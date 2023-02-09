2d60148137ef09468d9af6bd6f35ed1d9c96b1f5
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-09 22:06:49 +0100

    run the server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+//body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.listen(PORT,()=>{console.log("Server running at the PORT " + PORT)})
