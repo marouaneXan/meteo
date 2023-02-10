536f1a604aaec225dbd12b10602802d0d3a9a9bf
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-10 11:43:00 +0100

    check user authorization + protect weather route

--- client/navigators/index.js
+++ client/navigators/index.js
@@ -2,13 +2,31 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
 import Weathers from '../screens/Weathers';
 import RegisterPage from '../screens/Register';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
 const Stack = createNativeStackNavigator();
-const RootStack=()=>{
+const RootStack=({navigation})=>{
+    const [isAuthorized, setIsAuthorized] = useState(false);
+
+    useEffect(() => {
+        const checkAuthorization = async () => {
+            const name = await AsyncStorage.getItem('name');
+            if (name) {
+                setIsAuthorized(true);
+            } else {
+                navigation.navigate('Register');
+            }
+        };
+        checkAuthorization();
+    }, []);
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name='Register' component={RegisterPage} options={{ headerShown: false }}  />
-                <Stack.Screen name='Weather' component={Weathers} options={{ headerShown: false }}  />
+                {isAuthorized ? 
+                    <Stack.Screen name='Weather' component={Weathers} options={{ headerShown: false }}  />:
+                <>
+                    <Stack.Screen name='Register' component={RegisterPage} options={{ headerShown: false }}  />
+                </>}
             </Stack.Navigator>
         </NavigationContainer>
     )
